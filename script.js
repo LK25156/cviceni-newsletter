770bcb3812c081f600ecc56661915986f4ab702e
Lenka Koubková <lvseteckova@gmail.com>
2025-04-19 14:20:27 +0200

    přidání html,styly a js

---
+++ script.js
@@ -0,0 +1,9 @@
+const formular = document.querySelector("form");
+const odebirat = (event) => {
+  event.preventDefault()
+  const input = document.querySelector("input")
+  const email = input.value
+  formular.textContent = `Děkujeme za váš zájem. Těšíme se na novinky ze světa frontendu a UX na vaší adrese ${email}.`
+};
+
+formular.addEventListener("submit", odebirat);
